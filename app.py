aa1a66d73f3b46d158595d5fc9bdb5676c1e1f9b
NEDuck <cj96cj9696cj@gmail.com>
2023-01-03 10:20:44 +0800

    將主頁基本面板做好

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask, render_template, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_mysqldb import MySQL, MySQLdb
-
-from sqlalchemy.sql import func
+from data import operators, materials
 
 app = Flask(__name__)
 
 # app.secret_key = "caircocoders-ednalan"
-        
+
 # app.config['MYSQL_HOST'] = 'localhost'
 # app.config['MYSQL_USER'] = 'root'
 # app.config['MYSQL_PASSWORD'] = ''
@@ -17,19 +14,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+
+    return render_template('index.jinja', operators=operators, materials=materials)
 
 @app.route("/searchdata",methods=["POST","GET"])
 def searchdata():
     if request.method == 'POST':
         search_word = request.form['search_word']
         print(search_word)
-        cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        query = "SELECT * from tblprogramming WHERE title LIKE '%{}%' ORDER BY id DESC LIMIT 20".format(search_word)
-        cur.execute(query)
-        programming = cur.fetchall()
-    return jsonify({'data': render_template('response.html', programming=programming)})
+        # cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        # query = "SELECT * from tblprogramming WHERE title LIKE '%{}%' ORDER BY id DESC LIMIT 20".format(search_word)
+        # cur.execute(query)
+        # programming = cur.fetchall()
+    return jsonify({'data': render_template('response.html')})
 
 if __name__ == "__main__":
     app.run(debug=True)
-
--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,99 @@
 'use strict'
+const totalMaterialContainer = document.querySelector('#total-material-container')
+const userSelection = []
+
+function generateId(htmlElement) {
+    return `${htmlElement.dataset.operator}-${htmlElement.dataset.upgrade}-${(htmlElement.dataset.number)?htmlElement.dataset.number+"-":""}${htmlElement.dataset.level}`
+}
+function calculateTotal() {
+    const totalMaterials = {}
+
+    const addItem = (item) => {
+        if (!totalMaterials[item.id]) {
+            totalMaterials[item.id] = 0
+        }
+        totalMaterials[item.id] += Number(item.count)
+    }
+
+    for (const selection of userSelection) {
+        const token = [...selection.split('-')]
+        const operatorId = token[0]
+        const upgrade = token[1]
+        const level = token[3] || token[2]
+        const number = (token[3])?token[2]:0
+
+        const operator = operators[operatorId]
+
+        if      (upgrade === 'phase') {
+            for (const item of operator.phases[level].evolveCost) {
+                addItem(item)
+            }
+        }
+        else if (upgrade === 'skill') {
+            for (const item of operator.skills[number].levelUpCosts[level]) {
+                addItem(item)
+            }
+        }
+        else if (upgrade === 'uniequip') {
+            for (const item of Object.values(operator.uniequips[number].itemCost)[level]) {
+                addItem(item)
+            }
+        }
+    }
+
+    totalMaterialContainer.innerHTML = ''
+    for (const mid in totalMaterials) {
+        totalMaterialContainer.innerHTML += `
+        <div class="item-container">
+            <img src="${materials[mid].art}" alt="${materials[mid].name}">
+            <span>${totalMaterials[mid]}</span>
+        </div>
+        `
+    }
+}
+
+document.querySelectorAll('.select-ckb').forEach(ckb => {
+    ckb.addEventListener('click', () => {
+        if (ckb.checked) {
+            userSelection.push(generateId(ckb))
+        }
+        else {
+            const index = userSelection.indexOf(generateId(ckb))
+            if (index !== -1) {
+                userSelection.splice(index, 1)
+            }
+        }
+
+        calculateTotal()
+    })
+});
+
+document.querySelectorAll('.select-all-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+        document.querySelectorAll(`.select-ckb[data-operator="${btn.dataset.operator}"][data-upgrade="skill"][data-number="${btn.dataset.number}"]`).forEach((ckb) => {
+            if (!ckb.checked) {
+                ckb.click();
+            }
+        })
+    })
+});
+
+document.querySelectorAll('.unselect-all-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+        document.querySelectorAll(`.select-ckb[data-operator="${btn.dataset.operator}"][data-upgrade="skill"][data-number="${btn.dataset.number}"]`).forEach((ckb) => {
+            if (ckb.checked) {
+                ckb.click();
+            }
+        })
+    })
+});
+
+document.querySelector('#all-btn').addEventListener('click', () => {
+    document.querySelectorAll('.select-ckb').forEach((ckb) => {
+        if (!ckb.checked) {
+            ckb.checked = true;
+            userSelection.push(generateId(ckb))
+        }
+    })
+    calculateTotal()
+})
